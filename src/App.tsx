d201e83469c584f859328440076400ac815754f4
rohit <trycatchrohit@gmail.com>
2026-05-16 18:03:07 +0530

    portfolio uploaded

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 // import { useEffect } from 'react'
-import { Footer } from './components/Footer/Footer'
-import { Header } from './components/Header/Header'
-import { Main } from './components/Main/Main'
-import { GlobalStyle } from './styles/global'
-import 'react-toastify/dist/ReactToastify.css'
-import { myData } from '../src/data/data.js'
+import { Footer } from './components/Footer/Footer';
+import { Header } from './components/Header/Header';
+import { Main } from './components/Main/Main';
+import { GlobalStyle } from './styles/global';
+import 'react-toastify/dist/ReactToastify.css';
+import { myData } from '../src/data/data.js';
 
 
 function App() {
